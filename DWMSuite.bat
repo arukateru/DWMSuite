3ea8936289bc8ec68660875a4a70caec70380d04
Alcatel <84914212+arukateru@users.noreply.github.com>
2023-04-23 01:16:10 -0400

    Update DWMSuite.bat

--- DWMSuite.bat
+++ DWMSuite.bat
@@ -305,13 +305,16 @@ exit
 takeown /f %systemroot%\System32\Windows.UI.Logon.dll
 icacls %systemroot%\System32\Windows.UI.Logon.dll /grant Administrators:(F)
 ren %systemroot%\System32\Windows.UI.Logon.dll.bak Windows.UI.Logon.dll
-cls Done
+cls
+echo Done
 pause
 exit
 
 :ecl
+takeown /f %systemroot%\System32\Windows.UI.Logon.dll
+icacls %systemroot%\System32\Windows.UI.Logon.dll /grant Administrators:(F)
 ren %systemroot%\System32\Windows.UI.Logon.dll Windows.UI.Logon.dll.bak
-Done
+echo Done
 pause
 exit
 
